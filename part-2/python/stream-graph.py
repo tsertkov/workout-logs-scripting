526e4d85207a7e54d05b40e79dcdd00b2108d73d
Aleksandr Tsertkov <tsertkov@gmail.com>
2019-02-25 14:23:26 +0100

    Improve python graph

--- part-2/python/stream-graph.py
+++ part-2/python/stream-graph.py
@@ -24,12 +24,30 @@ df2["drops"] = 0
 for t in df2.index:
   df2["drops"][t] = reduce(lambda x, status: x - df2[status][t], statuses, 60)
 
-df2.plot.area(
+df2.plot.bar(
   figsize=(9, 3),
   legend=False,
+  stacked=True,
   ylim=(0, 60),
   fontsize=8,
+  width=1.0,
   color=["#24BC14", "#EA644A", "#ECD748", "#000000"]
 )
 
+plt.ylabel("Msg per minute")
+plt.xlabel("")
+locs, labels = plt.xticks()
+nlocs = list()
+nlabels = list()
+
+for i in range(len(locs)):
+  if i % 10 != 0: continue
+  nlocs.append(locs[i])
+  nlabels.append(labels[i])
+
+nlocs.append(179)
+nlabels.append(labels[-1])
+plt.xticks(nlocs, nlabels)
+
+plt.tight_layout()
 plt.savefig(sys.stdout.buffer)
